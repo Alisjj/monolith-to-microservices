98978629e413c4ac3085767c83d54c6bfc0cab70
Alisjj <aliyussajjads@gmail.com>
2022-08-31 12:39:36 +0100

    v6

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a584aa43bc7834dc2a1c4b33e438704b-1466677153.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
